58dd80c118dfc4ab55006fac3ab625d8bfb6f811
seonghoon-Oh <su5785@naver.com>
2022-02-10 10:44:39 +0900

    feat: Add clusterer to google map

--- src/components/map/MapDetail.js
+++ src/components/map/MapDetail.js
@@ -1,47 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import GoogleMapReact from "google-map-react";
+import MarkerClusterer from "@google/markerclustererplus";
 import styled, { ThemeProvider } from "styled-components";
 import axios from "axios";
 
 import ErrorPage from "../error/ErrorPage";
-import Marker from "./Marker";
-import theme from "../../styles/theme";
 import Modal from "../common/Modal";
-
-const getLatestPoint = array => {
-  const { latitude: lat, longitude: lng } = array[array.length - 1];
-
-  return { lat, lng };
-};
-
-const defaultProps = {
-  center: { lat: 37.508105, lng: 127.061341 },
-  zoom: 12,
-};
-
-const getPoints = id => {
-  return axios.get(`/map/${id}/points`);
-};
+import theme from "../../styles/theme";
 
 const MapDetail = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const [googleMapRef, setGoogleMapRef] = useState({ map: null, api: null });
 
-  const id = location.pathname.split("/")[2];
   const { REACT_APP_GOOGLE_API_KEY } = process.env;
+  const id = location.pathname.split("/")[2];
+  const defaultProps = {
+    center: { lat: 37.508105, lng: 127.061341 },
+    zoom: 12,
+  };
 
-  const handleMarkerClick = point => {
+  const handleApiLoaded = (map, api) => {
+    setGoogleMapRef({ map, api });
+  };
+
+  const handleMarkerClick = ({ latitude, longitude, placename }) => {
     navigate(
-      `point?latitude=${point.latitude}&longitude=${point.longitude}&placename=${point.placename}`
+      `point?latitude=${latitude}&longitude=${longitude}&placename=${placename}`
     );
+  };
+
+  const getLatestPoint = array => {
+    const { latitude: lat, longitude: lng } = array[array.length - 1];
+
+    return { lat, lng };
+  };
+
+  const getPoints = id => {
+    return axios.get(`/map/${id}/points`);
+  };
+
+  const onSuccess = data => {
+    if (data.mapPoints) {
+      const markers = data.mapPoints.map(point => {
+        const marker = new googleMapRef.api.Marker({
+          position: {
+            lat: point.latitude,
+            lng: point.longitude,
+          },
+          map: googleMapRef.map,
+        });
+
+        marker.addListener("click", () => {
+          handleMarkerClick(point);
+        });
+
+        return marker;
+      });
+
+      const cluster = new MarkerClusterer(googleMapRef.map, markers, {
+        imagePath:
+          "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+      });
+    }
   };
 
   const { isLoading, isError, error, data } = useQuery(
     "points",
     () => getPoints(id),
     {
+      onSuccess,
       select: data => data.data,
     }
   );
@@ -60,26 +90,17 @@ const MapDetail = () => {
           <GoogleMapReact
             bootstrapURLKeys={{ key: REACT_APP_GOOGLE_API_KEY }}
             defaultCenter={
-              data ? getLatestPoint(data.mapPoints) : defaultProps.center
+              data?.mapPoints
+                ? getLatestPoint(data.mapPoints)
+                : defaultProps.center
             }
             defaultZoom={defaultProps.zoom}
-          >
-            {data?.mapPoints.map(point => {
-              return (
-                <Marker
-                  key={point._id}
-                  id={point._id}
-                  lat={point.latitude}
-                  lng={point.longitude}
-                  count={point.photos.length}
-                  onClick={handleMarkerClick}
-                />
-              );
-            })}
-          </GoogleMapReact>
+            yesIWantToUseGoogleMapApiInternals
+            onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+          />
         </GoogleMapContainer>
       </Container>
-
+      useSuperCluster
       <Routes>
         <Route
           path="point"

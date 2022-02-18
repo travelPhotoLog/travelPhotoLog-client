8e7c2117d16583a923896ec22fd037a5d95e99e4
0jiiino <tnqkrtnqkr7756@gmail.com>
2022-02-19 00:52:50 +0900

    add: Add axios.interceptors

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,39 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URI,
-  withCredentials: true,
+  // withCredentials: true,
 });
+
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    config.headers.Authorization = `Bearer ${accessToken} ${refreshToken}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    const { data } = response;
+
+    if (data.accessToken) {
+      localStorage.setItem("accessToken", data.accessToken);
+    }
+
+    if (data.refreshToken) {
+      localStorage.setItem("refreshToken", data.refreshToken);
+    }
+
+    return response;
+  },
+  function (error) {
+    return error;
+  }
+);
 
 export default instance;

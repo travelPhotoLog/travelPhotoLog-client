174913597a41b6c8afaf69f94ec065bcfdc9c7ef
seonghoon-Oh <su5785@naver.com>
2022-02-19 02:28:37 +0900

    feat: Add download and load feature for image editor

--- src/components/photo/PhotoEditor.js
+++ src/components/photo/PhotoEditor.js
@@ -1,12 +1,40 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import ImageEditor from "@toast-ui/react-image-editor";
 import "tui-image-editor/dist/tui-image-editor.css";
 
 import theme from "../../styles/theme";
+import { StyledButton } from "../common/CommonStyle";
 
 const PhotoEditor = () => {
   const editorRef = useRef();
+
+  useEffect(() => {
+    const $buttons = document.querySelector(".tui-image-editor-header-buttons");
+    const $logo = document.querySelector(".tui-image-editor-header-logo");
+    const $loadContainer = document.querySelector("#loadButton");
+    const $loadButton = document.querySelector(".tui-image-editor-load-btn");
+
+    $loadButton.style.width = "0px";
+    $loadContainer.appendChild($loadButton);
+
+    $logo.remove();
+    $buttons.remove();
+  }, []);
+
+  const handleDownloadClick = () => {
+    const canvas = document.querySelector(".lower-canvas");
+
+    const link = document.createElement("a");
+    link.download = "download.png";
+    link.href = canvas.toDataURL();
+    link.click();
+    link.delete;
+  };
+
+  const handleFileClick = event => {
+    event.target.firstElementChild.click();
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -14,7 +42,7 @@ const PhotoEditor = () => {
         <ImageEditor
           ref={editorRef}
           includeUI={{
-            menu: ["shape", "filter", "crop"],
+            menu: ["shape", "filter", "crop", "text", "flip", "icon"],
             initMenu: "filter",
             uiSize: {
               width: "1000px",
@@ -30,6 +58,14 @@ const PhotoEditor = () => {
           }}
           usageStatistics={false}
         />
+        <Buttons>
+          <FileButton id="loadButton" onClick={handleFileClick}>
+            Load File
+          </FileButton>
+          <DownloadButton id="downloadButton" onClick={handleDownloadClick}>
+            Download
+          </DownloadButton>
+        </Buttons>
       </Container>
     </ThemeProvider>
   );
@@ -38,7 +74,41 @@ const PhotoEditor = () => {
 export default PhotoEditor;
 
 const Container = styled.div`
-  ${({ theme }) => theme.container.flexCenter};
+  ${({ theme }) => theme.container.flexCenterColumn};
   width: 100%;
-  height: 90vh;
+  height: 40%;
+  margin-top: 3rem;
+  position: relative;
+`;
+
+const Buttons = styled.div`
+  ${({ theme }) => theme.container.flexSpaceAround};
+  width: 300px;
+  height: 80px;
+  margin-top: 20px;
+`;
+
+const DownloadButton = styled(StyledButton)`
+  width: max-content;
+  border-radius: 20px;
+  background-color: orange;
+  color: white;
+  text-align: center;
+`;
+
+const FileButton = styled.div`
+  display: inline-flex;
+  padding: ${({ theme }) => theme.spacing.xl};
+  margin-left: ${({ theme }) => theme.spacing.xl};
+  margin-right: ${({ theme }) => theme.spacing.xl};
+  width: 8rem;
+  outline: none;
+  border: none;
+  font-weight: bold;
+  width: max-content;
+  border-radius: 20px;
+  background-color: orange;
+  color: white;
+  text-align: center;
+  cursor: pointer;
 `;
